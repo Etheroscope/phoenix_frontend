139e39d139210d84ce390c06c19c18718143a4b7
Rafael Ballestiero <rafa.ballestiero@gmail.com>
2017-10-26 23:25:13 +0100

    Removed frontend capabilities and started get_history api endpoint

--- apps/etheroscope_web/lib/etheroscope_web/router.ex
+++ apps/etheroscope_web/lib/etheroscope_web/router.ex
@@ -1,26 +1,13 @@
 defmodule EtheroscopeWeb.Router do
   use EtheroscopeWeb, :router
 
-  pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
-  end
-
   pipeline :api do
     plug :accepts, ["json"]
   end
 
   scope "/", EtheroscopeWeb do
-    pipe_through :browser # Use the default browser stack
-
-    get "/", PageController, :index
-  end
-
-  # Other scopes may use custom stacks.
-  scope "/api", EtheroscopeWeb do
     pipe_through :api
+
+    get "/history/:contract_address/:method", ContractController, :history
   end
 end
