dd5864b30a0fe684ca4c073365a9a410cf582f74
Kian Mayne <git@kianmayne.co.uk>
2017-11-02 18:13:13 +0000

    Improved style & Etheroscope fetch_* contracts

--- apps/etheroscope/lib/etheroscope/contract/contract.ex
+++ apps/etheroscope/lib/etheroscope/contract/contract.ex
@@ -1,4 +1,5 @@
 defmodule Etheroscope.Contract do
+  @api_base_url "https://api.etherscan.io"
   @allowed_types [
     "uint",
     "uint8",
@@ -23,26 +24,31 @@ defmodule Etheroscope.Contract do
   end
 
   def fetch_contract(contract_address) do
-    IO.inspect Application.get_env(:etheroscope, :etherscan_api_key)
-    url = "https://api.etherscan.io/api?module=contract&action=getabi&address="
-      <> contract_address <> "&apikey=" <> Application.get_env(:etheroscope, :etherscan_api_key)
-    {:ok, response} = HTTPoison.get(url)
-    response_body = Poison.decode!(response.body)
+    api_key = Application.get_env(:etheroscope, :etherscan_api_key)
+    url = "#{@api_base_url}/api?module=contract&action=getabi&address=#{contract_address}&apikey=#{api_key}"
+    case HTTPoison.get(url) do
+      {:error, msg} -> {:error, msg}
+      {:ok, response} ->
+        response_body = Poison.decode!(response.body)
 
-    if (response_body["result"] == "") do
-      %{:variables => [], :error => "No ABI found"}
-    else
-      contract_abi = Poison.decode!(response_body["result"])
-      variables = Enum.filter(contract_abi, &abi_item_is_variable/1)
-      var_names = Enum.map(variables, fn (var) -> var["name"] end);
-      %{:variables => var_names}
+        if (response_body["result"] == "") do
+          {:ok, %{:variables => [], :error => "No ABI found"}}
+        else
+          contract_abi = Poison.decode!(response_body["result"])
+          variables = Enum.filter(contract_abi, &abi_item_is_variable/1)
+          var_names = Enum.map(variables, fn (var) -> var["name"] end);
+          {:ok, %{:variables => var_names}}
+        end
     end
   end
 
   def fetch_history(contract_address) do
-    %{
-      :test => "Hello world (history)",
-      :contract_address => contract_address
+    {
+      :ok,
+      %{
+        :test => "Hello world (history)",
+        :contract_address => contract_address
+      }
     }
   end
 end
