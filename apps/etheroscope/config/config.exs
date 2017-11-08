5e80cac493c38a186af1babf47261661281f6218
Rafael Ballestiero <rafa.ballestiero@gmail.com>
2017-11-08 16:14:13 +0000

    Removed local.template logic and added instructions in README

--- apps/etheroscope/config/config.exs
+++ apps/etheroscope/config/config.exs
@@ -5,12 +5,4 @@ config :ethereumex,
   host: "parity.etheroscope.info",
   port: 8545
 
-config :etheroscope,
-  etherscan_api_key: "0000000000000" # Set this in backend-phoenix/config/local.exs
-
 import_config "#{Mix.env}.exs"
-
-local_config = "local.exs"
-if (File.exists?(Path.join(Path.dirname(__ENV__.file), local_config))) do
-  import_config local_config
-end
