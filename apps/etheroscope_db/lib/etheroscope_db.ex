ae16b4536203a5df6eb6f7f4eed27b6906906b3a
Rafael Ballestiero <rafa.ballestiero@gmail.com>
2017-11-08 18:39:14 +0000

    Started Parity wrapper with tests

--- apps/etheroscope_db/lib/etheroscope_db.ex
+++ apps/etheroscope_db/lib/etheroscope_db.ex
@@ -22,4 +22,19 @@ defmodule EtheroscopeDB do
       password: Application.get_env(:etheroscope_db, :password)
     }
   end
+
+  def fetch_filter(id), do: fetch("filter/#{id}")
+  def write_filter(id, params), do: write("filter/#{id}", params)
+
+  # HELPERS
+  defp fetch(id), do: Couchdb.Connector.get(db_props(), id)
+  defp write(id, params) do
+    with {:error, err} <- Couchdb.Connector.create(db_props(), params, id),
+         %{payload: %{"error" => err_msg}} <- err
+    do
+      {:error, err_msg}
+    else
+      {:ok, _resp} -> {:ok, id}
+    end
+  end
 end
--- apps/etheroscope_eth/test/etheroscope_eth_test.exs
+++ apps/etheroscope_eth/test/etheroscope_eth_test.exs
@@ -1,8 +1,4 @@
 defmodule EtheroscopeEthTest do
   use ExUnit.Case
   doctest EtheroscopeEth
-
-  test "greets the world" do
-    assert EtheroscopeEth.hello() == :world
-  end
 end
