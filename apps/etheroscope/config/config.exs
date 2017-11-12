018ef3c74438c753c11bd021f0934ba3a7e120a9
Rafael Ballestiero <rafa.ballestiero@gmail.com>
2017-11-12 17:22:55 +0000

    Added a test rpc container to docker-compose

--- apps/etheroscope/config/config.exs
+++ apps/etheroscope/config/config.exs
@@ -4,8 +4,3 @@ config :etheroscope,
   etherscan_api_key: "0000000000000" # Set this in backend-phoenix/config/local.exs
 
 import_config "#{Mix.env}.exs"
-
-local_config = "local.exs"
-if (File.exists?(Path.join(Path.dirname(__ENV__.file), local_config))) do
-  import_config local_config
-end
