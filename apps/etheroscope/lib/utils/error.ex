804df1901a5028f555f24b73093726f3b1582a8a
Rafael Ballestiero <rafa.ballestiero@gmail.com>
2017-11-16 14:16:43 +0000

    Improved utils

--- apps/etheroscope/lib/utils/error.ex
+++ apps/etheroscope/lib/utils/error.ex
@@ -4,16 +4,11 @@ defmodule Etheroscope.Util.Error do
   This module will also serve to handle any potential errors and report them correctly
   and accurately
   """
-  @type t :: {:error, map() | binary() | nonempty_list()}
+  @type t :: {:error, atom() | %{atom() => string()} | nonempty_list()}
 
-  defmacro handle_error(error_msg, do: block) do
-    quote do
-      try do
-        unquote(block)
-      rescue
-        e in RuntimeError -> Logger.error(unquote(error_msg) <> " -- " <> e.message)
-      end
-    end
-  end
+  defmacro build_error(err_type) when is_atom(err_type), do: {:error, err_type}
+  defmacro build_error(err_msg) when is_binary(err_msg), do: {:error, %{msg: err_msg}}
 
 end
+
+defmodule Etheroscope.Util.BadArgError, do: defexception message: "Bad argument passed as input"
--- apps/etheroscope/lib/utils/hex.ex
+++ apps/etheroscope/lib/utils/hex.ex
@@ -4,17 +4,19 @@ defmodule Etheroscope.Util.Hex do
   functions to hex values returned from the Parity client.
   """
 
-  @spec from_hex(binary()) :: integer()
-  def from_hex("0x" <> hex), do: String.to_integer(hex, 16)
+  @spec from_hex!(binary()) :: integer()
+  def from_hex!("0x" <> hex), do: String.to_integer(hex, 16)
+  def from_hex!(_), do: raise Etheroscope.Util.BadArgError
+  @spec from_hex({atom(), binary()}) :: {atom(), any()}
+  def from_hex({:ok, "0x" <> hex}), do: {:ok, String.to_integer(hex, 16)}
+  # propragate the error
+  def from_hex({:error, msg}), do: {:error, msg}
 
-  @spec to_hex(integer) :: binary()
-  def to_hex(num), do: "0x#{Integer.to_string(num, 16)}"
-
-  @spec apply_to_hex(((integer(), integer()) -> integer()), binary(), integer()) :: binary()
-  def apply_to_hex(op, hex, num) do
-    hex
-      |> from_hex
-      |> op.(num)
-      |> to_hex
-  end
+  @spec to_hex!(integer) :: binary()
+  def to_hex!(num) when is_integer(num), do: "0x#{Integer.to_string(num, 16)}"
+  def to_hex!(_),                        do: raise Etheroscope.Util.BadArgError
+  @spec to_hex({atom(), integer()}) :: {atom(), any()}
+  def to_hex({:ok, num}), do: {:ok, "0x#{Integer.to_string(num, 16)}"}
+  # propragate the error
+  def to_hex({:error, msg}), do: {:error, msg}
 end
