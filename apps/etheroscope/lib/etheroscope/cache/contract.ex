1a45e048b56694a9417c9674172d9546fc388d2d
Rafael Ballestiero <rafa.ballestiero@gmail.com>
2018-01-11 17:43:24 +0000

    Fixed contract ets cache

--- apps/etheroscope/lib/etheroscope/cache/contract.ex
+++ apps/etheroscope/lib/etheroscope/cache/contract.ex
@@ -12,14 +12,14 @@ defmodule Etheroscope.Cache.Contract do
       {:stale, blocks} -> get_new_blocks(address, blocks)
       nil              -> get_new_blocks(address, [])
       {:error, err}    -> Error.build_error(err)
-      res              -> res
+      {:ok, blocks}    -> {:ok, blocks, []}
     end
   end
 
   defp get_new_blocks(address, blocks) do
     resp = {status, new_blocks} = apply(next_storage_module(), :get_block_numbers, [address])
     if status == :ok do
-      Cache.add_elem_with_expiration(:contract_blocks, {address, blocks ++ new_blocks, new_blocks}, default_ttl())
+      Cache.add_elem_with_expiration(:contract_blocks, {address, blocks ++ new_blocks}, default_ttl())
     end
     {status, blocks ++ new_blocks, new_blocks}
   end
